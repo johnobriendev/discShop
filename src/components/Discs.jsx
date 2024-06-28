d012f207d009630bc056be9a6063eb024287154f
John O'Brien <johnobrien.dev@gmail.com>
2024-06-28 09:49:59 -0400

    add conditional loading message to shop

--- src/components/Discs.jsx
+++ src/components/Discs.jsx
@@ -4,26 +4,34 @@ import getDiscs from "../services/fetch";
 
 const Discs = () => {
   const [discs, setDiscs] = useState([]);
-  
+  const [loading, setLoading] = useState(true); // Add loading state
+
   useEffect(() => {
     const fetchDiscs = async () => {
       try {
         const data = await getDiscs();
         setDiscs(data);
-       
       } catch (err) {
-       
+        console.error(err);
+      } finally {
+        setLoading(false); // Set loading to false after fetching data
       }
     };
 
     fetchDiscs();
   }, []);
   
+  
   return(
     <div className="flex flex-col items-center sm:grid sm:grid-cols-2 sm:gap-4 md:grid md:grid-cols-3 md:gap-4">
-      {discs.map((disc) => (
-        <DiscCard  key={disc._id} disc={disc} />
-      ))}
+    {loading ? (
+        <div className="text-xl">Loading discs...</div> // Display loading message
+      ) : (
+        discs.map((disc) => (
+          <DiscCard key={disc._id} disc={disc} />
+        ))
+      )}
+
     </div>
   )
 }
